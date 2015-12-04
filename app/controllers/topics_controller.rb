4d6fd0c398adffde20df382729d00135a549225a
Michelle Li <limichelle21@gmail.com>
2015-12-03 23:14:39 -0800

    checkpoint 34 first commit

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,14 @@
+class TopicsController < ApplicationController
+
+	def index
+		@topics = Topic.all
+	end
+
+	def show
+		@topic = Topic.find(params[:id])
+	end
+
+	def new
+		@topic = Topic.new
+	end
+end
