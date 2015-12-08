d5c34532d464541fa9d2c73a9beb9ccf3e8a91bc
Michelle Li <limichelle21@gmail.com>
2015-12-07 17:13:52 -0800

    update for validates_uniqueness_of

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe User, type: :model do
 	it { should validate_length_of(:name).is_at_least(1)}
 
 	it { should validate_presence_of(:email)}
+	subject { User.new(name: 'test user')}
 	it { should validate_uniqueness_of(:email)}
 	it { should validate_length_of(:email).is_at_least(3)}
 	it { should allow_value("user@bloccit.com").for(:email)}
