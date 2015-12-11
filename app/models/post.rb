127fa0cdbc7aa685b46c9991cc0614a5d85ff6f6
Michelle Li <limichelle21@gmail.com>
2015-12-10 20:25:20 -0800

    assignment 29 scope working solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,16 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
-	default_scopt { order('created_at DESC')}
+	default_scope { order('created_at DESC')}
 
 	validates :title, length: { minimum: 5}, presence: true
 	validates :body, length: { minimum: 20}, presence: true
 	validates :topic, presence: true
 	validates :user, presence: true
+
+	scope :ordered_by_title, -> { order('title ASC')}
+
+	scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
+
 
 end
