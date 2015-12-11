c53cba2c624e3bbc68f812c57db9ea6b44d1d6e0
Michelle Li <limichelle21@gmail.com>
2015-12-10 22:47:27 -0800

    checkpoint 40 second commit updated topics_controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 
-	default_scopt { order('created_at DESC')}
+	default_scope { order('created_at DESC')}
 
 	validates :title, length: { minimum: 5}, presence: true
 	validates :body, length: { minimum: 20}, presence: true
