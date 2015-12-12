600a24adde85239e5e89ae1a9dfc10074a4f61d8
Michelle Li <limichelle21@gmail.com>
2015-12-11 22:08:22 -0800

    assignment 40 moderator first commit. rspec error on returns http redirect to topics_path

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,8 @@ class TopicsController < ApplicationController
 
 	before_action :require_sign_in, except: [:index, :show]
 	before_action :authorize_user, except: [:index, :show]
+	
+
 	def index
 		@topics = Topic.all
 	end
@@ -62,8 +64,8 @@ class TopicsController < ApplicationController
 	end
 
 	def authorize_user
-		unless current_user.admin?
-			flash.now[:alert] = "You must be an admin to do that."
+		unless current_user.admin? || current_user.moderator?
+			flash.now[:alert] = "You must be an admin or moderator to do that."
 			redirect_to topics_path
 		end
 	end
