929258173954ae79a1a167325be2cf0f2b0a8808
Michelle Li <limichelle21@gmail.com>
2015-12-18 20:17:54 -0800

    checkpoint 43 votes model and seeded votes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,22 +2,23 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 	let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password")}
-	it { should have_many(:posts)}
-	it { should have_many(:comments)}
+	it { should have_many(:posts) }
+	it { should have_many(:comments) }
+	it { should have_many(:votes) }
 
-	it { should validate_presence_of(:name)}
-	it { should validate_length_of(:name).is_at_least(1)}
+	it { should validate_presence_of(:name) }
+	it { should validate_length_of(:name).is_at_least(1) } 
 
-	it { should validate_presence_of(:email)}
-	subject { User.new(name: 'test user')}
-	it { should validate_uniqueness_of(:email)}
-	it { should validate_length_of(:email).is_at_least(3)}
-	it { should allow_value("user@bloccit.com").for(:email)}
-	it { should_not allow_value("userbloccit.com").for(:email)}
+	it { should validate_presence_of(:email) }
+	subject { User.new(name: 'test user') }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_length_of(:email).is_at_least(3) }
+	it { should allow_value("user@bloccit.com").for(:email) }
+	it { should_not allow_value("userbloccit.com").for(:email) }
 
-	it { should validate_presence_of(:password)}
-	it { should have_secure_password}
-	it { should validate_length_of(:password).is_at_least(6)}
+	it { should validate_presence_of(:password) }
+	it { should have_secure_password }
+	it { should validate_length_of(:password).is_at_least(6) }
 
 
 	describe "attributes" do
