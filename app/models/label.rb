41f87956c69f1a36ff90399edc1ea228f63c17ee
Michelle Li <limichelle21@gmail.com>
2015-12-23 19:56:12 -0800

    checkpoint 42 label working solution

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,7 +1,7 @@
 class Label < ActiveRecord::Base
 	has_many :labelings
 	has_many :topics, through: :labelings, source: :labelable, source_type: :Topic
-	has_many :posts, through: :labelings, source: :labelable, source_type: :Topic
+	has_many :posts, through: :labelings, source: :labelable, source_type: :Post
 
 
 	def self.update_labels(label_string)
