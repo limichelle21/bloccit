ce5f23bd38242d424c9f99dff0f90f4470ab94ec
Michelle Li <limichelle21@gmail.com>
2015-12-28 21:07:43 -0800

    checkpoint 45 profile working solution

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe CommentsController, type: :controller do
 	let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user)}
 	let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user)}
 
+	let(:my_topic) { create(:topic) }
+	let(:my_user) { create(:user) }
+	let(:other_user) { create(:user) }
+	let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+
+
 context "guest" do
 	describe "POST create" do
 		it "redirects the user to the sign in view" do
